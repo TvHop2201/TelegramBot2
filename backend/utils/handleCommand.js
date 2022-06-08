78f2d7146502aec2949880d36547359bd7ccb771
TvHop2201 <muc.tvh.2201@gmail.com>
2022-06-08 11:16:44 +0700

    ok

--- backend/utils/handleCommand.js
+++ backend/utils/handleCommand.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 const chatModel = require('../model/chat')
 const userModel = require('../model/user')
 const pointMessageModel = require('../model/pointMessage')
-const { urlencoded } = require('express')
 
 const telegramBot = `http://api.telegram.org/bot${process.env.BOT_TOKEN}`
 
@@ -48,7 +47,10 @@ class HandleCommand {
             this.handleThankCommand(text, chatId, fromId)
         }
         if (text.split(' ')[0] === 'point') {
-            this.handleListCommand(text, chatId)
+            this.handlePointCommand(text, chatId)
+        }
+        if (text.split(' ')[0] === 'gift') {
+            this.handleGiftCommand(text, chatId, fromId)
         }
     }
 
@@ -56,67 +58,47 @@ class HandleCommand {
         let [thankCommand, pointUser, ...pointMessage] = text.split(' ')
         pointMessage = pointMessage.join(' ')
 
-        const date = Date.now()
-
         if (pointUser.charAt(0) === '@') {
             pointUser = pointUser.split('@')[1]
             let data = await userModel.findOne({ userName: pointUser })
             if (data.length === 0) {
                 let text = 'không tồn tại người dùng'
-                let textEncode = encodeURI(text)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: text,
-                    date: date
-                })
+                this.sendText(text, chatId)
             } else {
-                await userModel.findOneAndUpdate({ userName: pointUser }, { point: data.point + 1 })
-                await userModel.findOneAndUpdate({ fromId: fromId111 }, { point: point - 1 })
-                await pointMessageModel.create({
-                    id: data.fromId,
-                    message: pointMessage
-                })
-                let text = `user : ${data.userName} đang có ${data.point + 1}`
-                let textEncode = encodeURI(text)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: text,
-                    date: date
-                })
+                if (data.point <= 1) {
+                    await userModel.findOneAndUpdate({ userName: pointUser }, { point: data.point + 1 })
+                    await userModel.findOneAndUpdate({ fromId: fromId111 }, { $inc: { point: -1 } })
+                    await pointMessageModel.create({
+                        id: data.fromId,
+                        message: pointMessage
+                    })
+                    let text = `user : ${data.userName} đang có ${data.point + 1} point`
+                    this.sendText(text, chatId)
+                } else {
+                    let text = 'Không Đủ Số Điểm Để Tặng'
+                    this.sendText(text, chatId)
+                }
             }
 
         } else {
             let data = await userModel.findOne({ firstName: pointUser })
             if (!data) {
                 let text = 'không tồn tại người dùng'
-                let textEncode = encodeURI(text)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: text,
-                    date: date
-                })
+                this.sendText(text, chatId)
             } else {
-                await userModel.findOneAndUpdate({ firstName: pointUser }, { point: data.point + 1 })
-                await userModel.findOneAndUpdate({ fromId: fromId111 }, { point: point - 1 })
-                await pointMessage.create({
-                    id: data.fromId,
-                    message: pointMessage
-                })
-                let text = `user : ${data.firstName} đang có ${data.point + 1}`
-                let textEncode = encodeURI(text)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: text,
-                    date: date
-                })
+                if (data.point <= 1) {
+                    await userModel.findOneAndUpdate({ firstName: pointUser }, { point: data.point + 1 })
+                    await userModel.findOneAndUpdate({ fromId: fromId111 }, { $inc: { point: -1 } })
+                    await pointMessageModel.create({
+                        id: data.fromId,
+                        message: pointMessage
+                    })
+                    let text = `user : ${data.firstName} đang có ${data.point + 1} point`
+                    this.sendText(text, chatId)
+                } else {
+                    let text = 'Không Đủ Số Điểm Để Tặng'
+                    this.sendText(text, chatId)
+                }
             }
         }
 
@@ -124,77 +106,102 @@ class HandleCommand {
     async handlePointCommand(text, chatId) {
         let [pointCommand, pointUser] = text.split(' ')
         if (!pointUser) {
-            let pData = await (await userModel.find().sort({ point: -1 })).limit(10)
-            let textOut = ''
+            let pData = await userModel.find().sort({ point: -1 }).limit(10)
+            let text = ''
             for (const index of pData) {
-                textOut = textOut + index.userName + index.firstName + " : " + index.point + "\n"
+                text = text + index.userName + " - " + index.firstName + " : " + index.point + "\n"
             }
-            let textEncode = encodeURI(textOut)
-            await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-            await chatModel.create({
-                fromId: 11111111,
-                chatId: chatId,
-                text: textOut,
-                date: date
-            })
+            this.sendText(text, chatId)
+        } else {
+            if (pointUser.charAt(0) === '@') {
+                pointUser = pointUser.split('@')[1]
+                let data = await userModel.findOne({ userName: pointUser })
+                if (!data) {
+                    let text = 'không tồn tại người dùng'
+                    this.sendText(text, chatId)
+                } else {
+                    let data2 = await pointMessageModel.find({ id: data.fromId }).limit(10)
+                    let text = ''
+                    for (const index of data2) {
+                        text = text + '1 point ' + new Date(index.Date).toLocaleDateString() + ' : ' + index.message + "\n"
+                    }
+                    this.sendText(text, chatId)
+                }
+
+            } else {
+                let data = await userModel.findOne({ firstName: pointUser })
+                if (!data) {
+                    let text = 'không tồn tại người dùng'
+                    this.sendText(text, chatId)
+                } else {
+                    let data2 = await pointMessageModel.find({ id: data.fromId }).limit(15)
+                    let text = ''
+                    for (const index of data2) {
+                        text = text + '1 point ' + new Date(index.Date).toLocaleDateString() + ' : ' + index.message + "\n"
+                    }
+                    this.sendText(text, chatId)
+                }
+            }
+
+        }
+
+    }
+    async handleGiftCommand(text, chatId, fromId111) {
+        let [giftCommand, pointUser, numPoint] = text.split(' ')
+        numPoint = parseInt(numPoint)
+        if (Number.isNaN(numPoint)) {
+            let text = 'Wrong point !!!'
+            this.sendText(text, chatId)
+            return 0;
         }
         if (pointUser.charAt(0) === '@') {
             pointUser = pointUser.split('@')[1]
             let data = await userModel.findOne({ userName: pointUser })
             if (!data) {
-                let text = 'không tồn tại người dùng'
-                let textEncode = encodeURI(text)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: text,
-                    date: date
-                })
+                let text = 'không tồn tại người dùng !!!'
+                this.sendText(text, chatId)
             } else {
-                let data2 = pointMessageModel.find({ id: data.fromId })
-                let textOut = ''
-                for (const index of data2) {
-                    textOut = textOut + '1 point ' + new Date(index.Date).toDateString() + ' : ' + index.message + "\n"
+                if (data.point >= numPoint) {
+                    await userModel.findOneAndUpdate({ userName: pointUser }, { point: data.point + numPoint })
+                    await userModel.findOneAndUpdate({ fromId: fromId111 }, { $inc: { point: - numPoint } })
+                    let text = `user : ${data.userName} đang có ${data.point + 1} point`
+                    this.sendText(text, chatId)
+                } else {
+                    let text = 'Không Đủ Số Điểm Để Tặng'
+                    this.sendText(text, chatId)
                 }
-                let textEncode = encodeURI(textOut)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: textOut,
-                    date: date
-                })
             }
-
         } else {
             let data = await userModel.findOne({ firstName: pointUser })
             if (!data) {
                 let text = 'không tồn tại người dùng'
-                let textEncode = encodeURI(text)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: text,
-                    date: date
-                })
+                this.sendText(text, chatId)
             } else {
-                let data2 = pointMessageModel.find({ id: data.fromId })
-                let textOut = ''
-                for (const index of data2) {
-                    textOut = textOut + '1 point ' + new Date(index.Date).toDateString() + ' : ' + index.message + "\n"
+                if (data.point >= numPoint) {
+                    await userModel.findOneAndUpdate({ firstName: pointUser }, { point: data.point + 1 })
+                    await userModel.findOneAndUpdate({ fromId: fromId111 }, { $inc: { point: -1 } })
+                    let text = `user : ${data.firstName} đang có ${data.point + 1} point`
+                    this.sendText(text, chatId)
+                } else {
+                    let text = 'Không Đủ Số Điểm Để Tặng'
+                    this.sendText(text, chatId)
                 }
-                let textEncode = encodeURI(textOut)
-                await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
-                await chatModel.create({
-                    fromId: 11111111,
-                    chatId: chatId,
-                    text: textOut,
-                    date: date
-                })
             }
         }
+
+    }
+
+
+    async sendText(text, chatId) {
+        let textEncode = encodeURI(text)
+        let textBr = text.replaceAll('\n', '<br/>')
+        await axios.post(`${telegramBot}/sendMessage?chat_id=${chatId}&text=${textEncode}`)
+        await chatModel.create({
+            fromId: 11111111,
+            chatId: chatId,
+            text: textBr,
+            date: Date.now()
+        })
     }
 }
 
